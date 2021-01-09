f795a173aa0255ea9189e011ce292f0992fd7ac1
Martialis39 <mart_lepanen@hotmail.com>
2021-01-09 15:42:16 +0200

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      spacing: {
+        "4px": "4px",
+        "8px": "8px",
+        "16px": "16px",
+        "32px": "32px",
+        "64px": "64px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
